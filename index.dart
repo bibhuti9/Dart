e7cd7aaaed4dff354441ad7f3de9854e67d1c588
bibhutiswain9 <144211577+bibhutiswain9@users.noreply.github.com>
2024-01-20 12:57:47 +0530

    add list

--- index.dart
+++ index.dart
@@ -1,15 +1,10 @@
-class RequiredClass {
-  double radious;
-  // required here is mandatory for radious
-  RequiredClass({required this.radious});
-
-  /*
-    return : {double} this.radious
-  */
-  double get area => 3.14 * this.radious;
-}
-
 void main() {
-  var objOfRequired = RequiredClass(radious: 20);
-  print(objOfRequired.area);
+  List<int> lists = [];
+  lists.add(2);
+  lists.add(3);
+  lists.add(4);
+  lists.add(5);
+  for (int list in lists) {
+    print("${list} is ${list % 2 == 0 ? "Even" : "Odd"}");
+  }
 }
