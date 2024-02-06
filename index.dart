24a1f1fc1484292e488dc40809ce067d9b8ce628
bibhutiswain9 <bibhutiswain990@gmail.com>
2024-02-06 17:55:29 +0530

    add future

--- index.dart
+++ index.dart
@@ -67,36 +67,8 @@ class students {
 }
 
 Future<void> main() async {
-  var obj = new students();
-  studentss.map((e) => obj.setStudent(e)).toList();
-  List<Map<String, dynamic>> list = obj.getStudents();
-
-  final numbers = <int>[
-    1,
-    2,
-    4,
-    5,
-  ];
-
-  List<String> number2 = numbers
-      .map((element) => "${element} ${element % 2 == 0 ? "Event" : "Odd"}")
-      .toList();
-
-  int count = numbers.reduce((first, second) => first + second);
-  print(number2);
-
-  var future = Future.delayed(Duration(seconds: 1), () => 2);
-  future.then((value) => print(value));
-  future.whenComplete(() => print("Afer"));
+  print("Fuck you1");
+  var result = await Future<int>.delayed(Duration(seconds: 3), () => 2);
+  print(result);
+  print("Fuck you3");
 }
-
-
-/**
- * Var
- * Const
- * List
- * Map
- * Set
- * Dynamic
- * Final
-*/
