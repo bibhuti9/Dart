8a596c6ae4ec0d5be1e93a0e21d0203e87608225
bibhutiswain9 <bibhutiswain990@gmail.com>
2024-03-03 12:28:21 +0530

    play with variables

--- index.dart
+++ index.dart
@@ -1,74 +1,14 @@
-var studentss = [
-  {
-    "id": 1,
-    "profile": {"name": "bibhuti"},
-    "comments": 20,
-    "posts": {
-      "post": [
-        {"name": "Today"},
-        {"name": "Next day"},
-        {"name": "Previous"},
-      ],
-      "length": 3
-    }
-  },
-  {
-    "id": 2,
-    "profile": {"name": "bibhuti"},
-    "comments": 20,
-    "posts": {
-      "post": [
-        {"name": "Today"},
-        {"name": "Next day"},
-        {"name": "Previous"},
-      ],
-      "length": 3
-    }
-  },
-  {
-    "id": 3,
-    "profile": {"name": "bibhuti"},
-    "comments": 20,
-    "posts": {
-      "post": [
-        {"name": "Today"},
-        {"name": "Next day"},
-        {"name": "Previous"},
-      ],
-      "length": 3
-    }
-  },
-];
-
-Map<String, dynamic> addVal = {
-  "id": 4,
-  "profile": {"name": "bibhuti"},
-  "comments": 20,
-  "posts": {
-    "post": [
-      {"name": "Today"},
-      {"name": "Next day"},
-      {"name": "Previous"},
-    ],
-    "length": 3
-  }
-};
-
-class students {
-  List<Map<String, dynamic>> student = [];
-
-  setStudent(Map<String, dynamic> student) {
-    this.student.add(student);
-  }
-
-  getStudents() {
-    return this.student;
-  }
-}
-
-Future<void> main() async {
-  print("Fuck you1");
-  var result = await Future<int>.delayed(Duration(seconds: 3), () => 2);
-  print(result);
-  print("Fuck you3");
+void main() {
+  var name = "John";
+  String? dob;
+  String? myName = dob;
+  final name1 = "Bibhuti";
+  const name2 = "Bibhuti";
+  dynamic name3 = "Bibhuti";
+  print(name);
+  print(name1);
+  print(name2);
+  print(name3.runtimeType);
+  name3 = 5;
+  print(name3.runtimeType);
 }
